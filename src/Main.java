7520b0c77ab1979748c256854abe430ee2b3c23d
sabdo11 <sabdo@msudenver.edu>
2022-02-17 09:35:52 -0700

    made some change

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void  main(String [] args) {
-        Tree myFavoriteOakTree = new Tree (25, 5, TreeType.OAK);
-        Tree myFavoriteMapleTree = new Tree (25, 5, TreeType.MAPLE);
+        Tree myFavoriteOakTree = new Tree (120, 12, TreeType.OAK);
+        Tree myFavoriteMapleTree = new Tree (180, 30, TreeType.MAPLE);
+        Tree myFavoriteWalnutTree = new Tree (150, 18, TreeType.WALNUT);
+        myFavoriteOakTree.announceTalltree ();
+        myFavoriteMapleTree.announceTalltree ();
+        myFavoriteWalnutTree.announceTalltree ();
 
-        if (myFavoriteOakTree.heightFt > 100) {
-            System.out.print ("That's a tall");
-        }
-        if (myFavoriteMapleTree.heightFt > 100){
-            System.out.print ("That's a tall tree");
-        }
+        System.out.print (Tree.TRUNK_COLOR);
+        Tree.announceTree ();
     }
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,19 +1,32 @@
+import java.awt.*;
+
 public class Tree {
     double heightFt;
     double trunkDiameterInches;
     TreeType treeType;
+    static  Color TRUNK_COLOR = new Color (120, 51,0);
 
     // Constructor
-    Tree(double heightFt, double trunkDiameterInches, TreeType treeType ){
+    Tree(double heightFt, double trunkDiameterInches, TreeType treeType) {
         this.heightFt = heightFt;
         this.trunkDiameterInches = trunkDiameterInches;
         this.treeType = treeType;
 
     }
 
-   void grow(){
-       this.heightFt = this.heightFt + 10;
-       this.trunkDiameterInches = this.trunkDiameterInches +1;
+    void grow() {
+        this.heightFt = this.heightFt + 10;
+        this.trunkDiameterInches = this.trunkDiameterInches + 1;
 
+    }
+    static void announceTree(){
+        System.out.print ("look out for that " + TRUNK_COLOR + " tree!");
+    }
+
+
+    void announceTalltree() {
+        if (this.heightFt > 100) {
+            System.out.print ("That's a tall " + this.treeType + " tree! ");
+        }
     }
 }
