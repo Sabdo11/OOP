f08a3a3169106403aced0caeaaa33b1bcc47b394
sabdo11 <sabdo@msudenver.edu>
2022-02-17 10:18:22 -0700

    Data Encapsulation

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ public class Main {
         Tree myFavoriteOakTree = new Tree (120, 12, TreeType.OAK);
         Tree myFavoriteMapleTree = new Tree (180, 30, TreeType.MAPLE);
         Tree myFavoriteWalnutTree = new Tree (150, 18, TreeType.WALNUT);
-        myFavoriteOakTree.announceTalltree ();
-        myFavoriteMapleTree.announceTalltree ();
-        myFavoriteWalnutTree.announceTalltree ();
 
-        System.out.print (Tree.TRUNK_COLOR);
-        Tree.announceTree ();
+        System.out.print ( myFavoriteOakTree.getTreeType ());
+        System.out.print ( myFavoriteOakTree.getHeightFt ());
+        System.out.print ( myFavoriteOakTree.getTrunkDiameterInches ());
+
+
+        myFavoriteOakTree.grow();
+
     }
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,30 +1,45 @@
 import java.awt.*;
 
 public class Tree {
-    double heightFt;
-    double trunkDiameterInches;
-    TreeType treeType;
-    static  Color TRUNK_COLOR = new Color (120, 51,0);
+    private double heightFt;
+    private double trunkDiameterInches;
+    private TreeType treeType;
+   protected static  Color TRUNK_COLOR = new Color (120, 51,0);
 
     // Constructor
-    Tree(double heightFt, double trunkDiameterInches, TreeType treeType) {
+    public Tree(double heightFt, double trunkDiameterInches, TreeType treeType) {
         this.heightFt = heightFt;
         this.trunkDiameterInches = trunkDiameterInches;
         this.treeType = treeType;
 
     }
 
-    void grow() {
+    public double getHeightFt() {
+        return heightFt;
+    }
+    public double getTrunkDiameterInches(){
+        return trunkDiameterInches;
+    }
+
+    public void setTrunkDiameterInches(double trunkDiameterInches) {
+        this.trunkDiameterInches = trunkDiameterInches;
+    }
+
+    public TreeType getTreeType(){
+        return treeType;
+    }
+
+    public void grow() {
         this.heightFt = this.heightFt + 10;
         this.trunkDiameterInches = this.trunkDiameterInches + 1;
 
     }
     static void announceTree(){
-        System.out.print ("look out for that " + TRUNK_COLOR + " tree!");
+        System.out.print ("look out for that " + TRUNK_COLOR + " tree! " );
     }
 
 
-    void announceTalltree() {
+   public void announceTalltree() {
         if (this.heightFt > 100) {
             System.out.print ("That's a tall " + this.treeType + " tree! ");
         }
